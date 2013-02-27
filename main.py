35a98a81a1453cca62266df7997b532f9e6989a7
haumea <haumea@haumea-Satellite-M645.(none)>
2013-02-27 18:35:14 -0500

    restructure

---
+++ main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from lib.pidtrk import *
+
+
+def main():
+    try:
+        pid = os.fork()
+        if pid > 0:
+            sys.exit(0)
+    except OSError, e:
+        sys.exit(1)
+
+    os.chdir('/')
+    os.setsid()
+    os.umask(0)
+
+    try:
+        pid = os.fork()
+        if pid > 0:
+            sys.exit(0)
+    except OSError, e:
+        sys.exit(1)
+
+    ptrk = ProcessTrack()
+    ptrk.process_check()
+
+if __name__ == '__main__':
+    main()
