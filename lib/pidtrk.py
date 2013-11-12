f49bc7377bd1a2ff0e1196f7436568c5475fc834
Timmy Konick <konick781@gmail.com>
2013-11-12 03:21:03 -0500

    minor rev

--- lib/pidtrk.py
+++ lib/pidtrk.py
@@ -8,14 +8,15 @@ import os
 
 class ProcessTrack(object):
 
+    PIDFILE = os.environ['HOME'] + '/.pidtrk.txt'
+    PIDB = os.environ['HOME'] + '/.pidtrk.db'
+
     def process_check(self):
-        self.create_db()
-        db_list = self.create_db_list()
-        tagged_ps = list() 
-        try:
-            os.remove(os.environ['HOME'] + '/.pidtrk.txt')
-        except OSError:
-            pass 
+        self.create_db
+        db_list = self.create_db_list
+        tagged_ps = list()
+        if os.path.isfile(self.PIDFILE) 
+            os.remove(self.PIDFILE)
         while True:
             time.sleep(5)
             ps = [i for i in os.listdir('/proc') if i.isdigit()]
@@ -26,7 +27,7 @@ class ProcessTrack(object):
                             ps_name = f.read().strip('\n')
                         if ps_name not in tagged_ps:
                             tagged_ps.append(ps_name)
-                            with open(os.environ['HOME'] + '/.pidtrk.txt', 'a+') as f:
+                            with open(self.PIDFILE, 'a+') as f:
                                 f.write(ps_name + ': ' + time.ctime() + '\n')
                         if ps_name not in db_list:
                             self.enter_process(ps_name)
@@ -34,28 +35,26 @@ class ProcessTrack(object):
                     except AttributeError or IOError:
                         pass
 
+    @property
     def create_db(self):
-        con = sqlite3.connect(os.environ['HOME'] + '/.pidtrk.db')
+        con = sqlite3.connect(self.PIDB)
         with con:
             cur = con.cursor()
             cur.execute("CREATE TABLE IF NOT EXISTS Process(pidname TEXT, time TEXT)")
-        con.commit()
-        con.close()
- 
+        return
+
+    @property 
     def create_db_list(self):
-        con = sqlite3.connect(os.environ['HOME'] + '/.pidtrk.db')
+        con = sqlite3.connect(self.PIDB)
         with con:
             cur = con.cursor()
             cur.execute("SELECT * FROM Process")
             prclst = [i[0] for i in cur.fetchall()]
-        con.commit()
-        con.close()
         return prclst
 
     def enter_process(self, process):
-        con = sqlite3.connect(os.environ['HOME'] + '/.pidtrk.db')
+        con = sqlite3.connect(self.PIDB)
         with con:
             cur = con.cursor()
             cur.execute("INSERT INTO Process VALUES(?,?)", (process, time.ctime()))
-        con.commit()
-        con.close()
+        return
